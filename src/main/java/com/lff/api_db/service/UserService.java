39e74b3693ebdaaec82a537fd5132416887a4564
Lucas Freitas <lffreita@mapfre.com.br>
2026-06-09 10:25:01 -0300

    update: Padronized the log register

--- src/main/java/com/lff/api_db/service/UserService.java
+++ src/main/java/com/lff/api_db/service/UserService.java
@@ -17,22 +17,22 @@ public class UserService {
      private UserRepository repository;
 
      public Optional<UserDomain> getById(Long id){
-          System.out.println(String.format("====\n[  INFO  ](Get/ID) Buscando usuário: %d\n====", id));
+          System.out.println(String.format("[  INFO  ](Get/ID) Buscando usuário: %d", id));
           return this.repository.findById(id);
      }
 
      public List<UserDomain> getAll(){
-          System.out.println("====\n[  INFO  ](Get/All) Buscando todos os usuários\n====");
+          System.out.println("[  INFO  ](Get/All) Buscando todos os usuários");
           return this.repository.findAll();
      }
 
      public void deleteUser(Long id){
-          System.out.println(String.format("====\n[  WARN  ](Delete) Deletando usuário: %d\n====", id));
+          System.out.println(String.format("[  WARN  ](Delete) Deletando usuário: %d", id));
           this.repository.deleteById(id);
      }
 
      public UserDomain createUser(UserDomain user){
-          System.out.println(String.format("====\n[  INFO  ](Post) Criando usuário: %s\n====", user.getUsername()));
+          System.out.println(String.format("[  INFO  ](Post) Criando usuário: %s", user.getUsername()));
           return this.repository.save(user);
      }
 
@@ -46,10 +46,10 @@ public class UserService {
                userDB.setUsername(user.getUsername());
                userDB.setEmail(user.getEmail());
                userDB.setActive(user.getActive());
-               System.out.println(String.format("====\n[  INFO  ](Put/ID) Atualizando usuário completo: %d\n====", id));
+               System.out.println(String.format("[  INFO  ](Put/ID) Atualizando usuário completo: %d", id));
                return this.repository.save(userDB);
           } else {
-               System.out.println(String.format("====\n[  ERRO  ](Put/ID) Usuário não encontrado: %d\n====", id));
+               System.out.println(String.format("[  ERRO  ](Put/ID) Usuário não encontrado: %d", id));
                throw new IllegalArgumentException("Usuário não encontrado com ID: " + id);
           }
 
@@ -66,10 +66,10 @@ public class UserService {
                dto.username().ifPresent(userDB::setUsername);
                dto.email().ifPresent(userDB::setEmail);
                dto.active().ifPresent(userDB::setActive);
-               System.out.println(String.format("====\n[  INFO  ](Patch/ID) Atualizando usuário: %d\n====", id));
+               System.out.println(String.format("[  INFO  ](Patch/ID) Atualizando usuário: %d", id));
                return this.repository.save(userDB);
           } else {
-               System.out.println(String.format("====\n[  ERRO  ](Patch/ID) Usuário não encontrado: %d\n====", id));
+               System.out.println(String.format("[  ERRO  ](Patch/ID) Usuário não encontrado: %d", id));
                return null;
                }
           }
